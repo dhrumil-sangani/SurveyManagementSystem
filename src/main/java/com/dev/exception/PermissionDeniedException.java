3bfc49b01ee5f9f29cabffe8b0e57327c904ebb4
darshan.dave <darshan.dave@spec-india.com>
2023-08-17 15:54:19 +0530

    added repositories and changes in model

---
+++ src/main/java/com/dev/exception/PermissionDeniedException.java
@@ -0,0 +1,12 @@
+package com.dev.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class PermissionDeniedException extends RuntimeException {
+
+	public PermissionDeniedException(String action) {
+		super("Permission denied for action: " + action);
+	}
+}
