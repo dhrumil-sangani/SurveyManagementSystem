435e2ef314377f89e45d0c19f2205aef8de49fd3
rana-hardik1231 <hardik.rana@spec-india.com>
2023-08-23 11:45:48 +0530

    add web dependency

---
+++ src/main/java/com/spec/surveymanagementsystem/controller/AuthenticationController.java
@@ -0,0 +1,15 @@
+package com.spec.surveymanagementsystem.controller;
+import java.util.*;
+import com.spec.surveymanagementsystem.model.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+	@GetMapping("/user")
+	public String hello {
+	    return "Hello";
+	}
+}
