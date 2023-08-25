a0cf5226f1424822354a7c66b70a13e68de8c043
Hitesh Solanki <hitesh.solanki@specindia.com>
2023-08-25 11:21:19 +0530

    error solved

--- src/main/java/com/spec/surveymanagementsystem/model/Organization.java
+++ src/main/java/com/spec/surveymanagementsystem/model/Organization.java
@@ -1,6 +1,7 @@
 package com.spec.surveymanagementsystem.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.List;
@@ -13,12 +14,29 @@ import java.util.Date;
 @Table(name = "organizations")
 public class Organization {
 
-    @Id
+    /**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
     @Size(min = 1, max = 100)
     private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
 
     @Column(name = "status", nullable = false)
     private boolean status;
