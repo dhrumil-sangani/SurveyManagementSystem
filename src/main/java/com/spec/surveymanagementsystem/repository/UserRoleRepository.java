8be918945007a8ff66eccdb814b9965332dbfec5
dhrumil.sangani <dhrumil.sagani@gmail.com>
2023-09-19 15:04:15 +0530

    Delete unnecessary files

--- src/main/java/com/spec/surveymanagementsystem/repository/UserRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spec.surveymanagementsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.spec.surveymanagementsystem.model.UserRole;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-
-}
